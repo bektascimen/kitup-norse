bf410b3c4a68cb49916a730089e44517f634fcaf
bektascimen <bektascimen@gmail.com>
2026-05-05 02:37:44 +0300

    fix(eslint-config): drop unknown react-native env, document next consumer dep

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -1,3 +1,4 @@
+// Consumer must install `eslint-config-next` for `next/core-web-vitals` to resolve.
 module.exports = {
   extends: [require.resolve("./index.js"), "next/core-web-vitals"]
 };
--- packages/eslint-config/react-native.js
+++ packages/eslint-config/react-native.js
@@ -1,5 +1,4 @@
 module.exports = {
   extends: [require.resolve("./index.js")],
-  env: { "react-native/react-native": true },
   globals: { __DEV__: "readonly" }
 };
